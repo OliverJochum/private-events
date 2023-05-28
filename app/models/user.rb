7020b6273041f4de20756bf23dfb841c9b8d8e1a
Oliver Jochum <oliver.jochum@yahoo.com>
2023-05-28 18:47:10 +0200

    updated models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :authored_events, inverse_of: 'creator',class_name: 'Event'
+  has_many :authored_events, foreign_key: 'creator_id',class_name: 'Event'
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attended_events, through: :event_attendings
 end
